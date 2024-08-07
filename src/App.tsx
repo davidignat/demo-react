98b34696dda152e894afc361fc869bcb3b42fb57
David <rodroids@academiadeinfo.ro>
2024-08-07 11:17:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return <div></div>;
+}
+
+export default App;
